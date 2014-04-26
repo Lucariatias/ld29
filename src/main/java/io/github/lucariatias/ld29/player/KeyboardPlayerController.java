3e776f74baaef8ab63922a3fda5f580f05fbfa0c
Lucariatias <lucariatias@gmail.com>
2014-04-27 00:12:37 +0100

    Revert keyboard player controller intermediary angles

--- src/main/java/io/github/lucariatias/ld29/player/KeyboardPlayerController.java
+++ src/main/java/io/github/lucariatias/ld29/player/KeyboardPlayerController.java
@@ -27,7 +27,6 @@ public class KeyboardPlayerController extends PlayerController implements KeyLis
     @Override
     public void keyReleased(KeyEvent event) {
         switch (event.getKeyCode()) {
-            case VK_RIGHT:case VK_D:case VK_DOWN:case VK_S:case VK_LEFT:case VK_A:case VK_UP:case VK_W: setApproachingAngle(getPlayer().getAngle()); break;
             case VK_R: getPlayer().getLevel().reset(); break;
         }
     }
