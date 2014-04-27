68e5cfa6a0bde49fadd63033bcb723e45f9a8dc8
Lucariatias <lucariatias@gmail.com>
2014-04-27 09:34:05 +0100

    Add countdown

--- src/main/java/io/github/lucariatias/ld29/level/LevelPanel.java
+++ src/main/java/io/github/lucariatias/ld29/level/LevelPanel.java
@@ -13,11 +13,13 @@ public class LevelPanel extends JPanel {
     private Level level;
     private Camera camera;
     private boolean active;
+    private int countDown;
 
     public LevelPanel(Descent descent, Level level, Player player) {
         this.descent = descent;
         this.level = level;
         this.camera = new Camera(player);
+        this.countDown = 160;
     }
 
     public Camera getCamera() {
@@ -34,8 +36,11 @@ public class LevelPanel extends JPanel {
 
     public void onTick() {
         if (active) {
-            level.onTick();
-            camera.onTick();
+            countDown = countDown > 0 ? countDown - 1 : countDown;
+            if (countDown == 0) {
+                level.onTick();
+                camera.onTick();
+            }
         }
     }
 
@@ -47,6 +52,15 @@ public class LevelPanel extends JPanel {
         render(graphics);
         graphics2D.translate(camera.getLocation().getX(), camera.getLocation().getY());
         descent.getNotificationManager().render(graphics);
+        if (countDown > 0) {
+            graphics.setColor(Color.BLACK);
+            graphics.fillOval(288, 208, 64, 64);
+            graphics.setColor(Color.RED);
+            graphics.drawOval(288, 208, 64, 64);
+            if (countDown > 120) graphics.drawString("3", 320 - ((int) graphics.getFontMetrics().getStringBounds("3", graphics).getWidth() / 2), 244);
+            else if (countDown > 80) graphics.drawString("2", 320 - ((int) graphics.getFontMetrics().getStringBounds("2", graphics).getWidth() / 2), 244);
+            else if (countDown > 4) graphics.drawString("1", 320 - ((int) graphics.getFontMetrics().getStringBounds("1", graphics).getWidth() / 2), 244);
+        }
     }
 
     private void render(Graphics graphics) {
