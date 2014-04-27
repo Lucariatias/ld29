6799e180c25045eb6ded989829ff46722cd84004
Lucariatias <lucariatias@gmail.com>
2014-04-27 09:46:34 +0100

    Half turning speed

--- src/main/java/io/github/lucariatias/ld29/player/PlayerController.java
+++ src/main/java/io/github/lucariatias/ld29/player/PlayerController.java
@@ -2,7 +2,7 @@ package io.github.lucariatias.ld29.player;
 
 public abstract class PlayerController {
 
-    private static final int TURNING_SPEED = 10;
+    private static final int TURNING_SPEED = 5;
 
     private Player player;
 
